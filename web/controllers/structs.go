c78f3a608c4149aae62620423645be5198333c4c
we4tech <hasan83bd@gmail.com>
2013-08-05 01:16:01 +0600

    First commit

---
+++ web/controllers/structs.go
@@ -0,0 +1,34 @@
+package controllers
+
+import (
+	"models"
+	"github.com/gorilla/sessions"
+)
+
+type Form interface {
+	IsValid() (bool, error)
+}
+
+type AppController struct {
+	Store *sessions.CookieStore
+}
+
+type DataContext struct {
+	CurrentUser *models.User
+	LoggedIn bool
+	Notice   string
+
+	User     *models.User
+	Codes    []models.Code
+	Code 	 *models.Code
+	Err      string
+}
+
+type ActionResponse struct {
+	RedirectTo string
+	Render     string
+	Context    *DataContext
+	RenderText string
+	Code       int
+	Notice	 string
+}
